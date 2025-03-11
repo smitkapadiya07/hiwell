c8cada643559ff0568ac86b67bae1bb0d76d115f
gpatel11 <accoubntt@gmail.com>
2025-03-11 16:30:42 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Psychologicals from "./pages/psychological/psychologicals.jsx";
 
 function App() {
 
   return (
     <>
+
+        <Psychologicals />
+      {/*<Routes>*/}
+      {/*  <Route path="/" element={<Psychologicals />} />*/}
+      {/*</Routes>*/}
     </>
   )
 }
